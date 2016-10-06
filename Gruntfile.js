265bc5cc3154f6ef899e9bc80b53f67b36bc8623
MKS-Student <mks.student@hackreactor.com>
2016-10-05 19:48:49 -0700

    added to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
 
     nodemon: {
       dev: {
-        script: 'server.js',
+        script: '--watch app --watch server.js',
         options: {
           env: {
             PORT: '3300'
